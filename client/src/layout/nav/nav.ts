853c3a834e9f5fd9bced71cbd4cfc21cd13f4526
Xiaomin Guo <aislandmin@hotmail.com>
2025-07-31 15:25:03 -0400

    Implement persistence when updating resources using Angular template forms, CanDeactivate Route Guard, @ViewChild decorator, loading indicator, caching data in Angular services

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -4,6 +4,7 @@ import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastSerive } from '../../core/services/toast-serive';
 import { themes } from '../theme';
+import { BusyService } from '../../core/services/busy-service';
 
 @Component({
   selector: 'app-nav',
@@ -13,6 +14,7 @@ import { themes } from '../theme';
 })
 export class Nav implements OnInit {
   protected accountService = inject(AccountService);
+  protected busyService = inject(BusyService);
   protected creds: any = {};
   private router = inject(Router);
   private toast = inject(ToastSerive);
